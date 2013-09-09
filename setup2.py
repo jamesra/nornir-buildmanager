1735e01902331d82c1835912f2dbc668a5b913c5
James Anderson <james.r.andreson@utah.edu>
2013-09-09 12:11:25 -0700

    Setup tweaks

---
+++ setup2.py
@@ -0,0 +1,50 @@
+'''
+Created on Aug 30, 2013
+
+@author: James Anderson
+'''
+
+
+from ez_setup import use_setuptools
+from setuptools import setup, find_packages
+
+use_setuptools()
+
+required_packages = ["nornir_pools",
+                     "nornir_shared",
+                     "nornir_imageregistration",
+                    "numpy",
+                    "scipy",
+                    "matplotlib"]
+
+install_requires = ["nornir_pools",
+                    "nornir_shared",
+                    "nornir_imageregistration",
+                    "numpy",
+                    "scipy",
+                    "matplotlib"]
+
+packages = find_packages()
+
+provides = ["nornir_buildmanager"]
+
+dependency_links = ["git+http://github.com/jamesra/nornir-pools#egg=nornir_pools",
+                    "git+http://github.com/jamesra/nornir-shared#egg=nornir_shared",
+                    "git+http://github.com/jamesra/nornir-imageregistration#egg=nornir_imageregistration"]
+
+package_dir = {'nornir_buildmanager' : 'nornir_buildmanager'}
+data_files = {'nornir_buildmanager' : ['config/*.xml']}
+scripts = ['']
+
+setup(name='nornir_buildmanager',
+      version='1.0',
+      description="Scripts for the construction of 3D volumes from 2D image sets.",
+      author="James Anderson",
+      author_email="James.R.Anderson@utah.edu",
+      url="https://github.com/jamesra/nornir-buildmanager",
+      packages=packages,
+      package_data=data_files,
+      requires=required_packages,
+      install_requires=install_requires,
+      provides=provides,
+      dependency_links=dependency_links)
