76ffe53665226a828fa23fdc4d8970cbe65b8939
James Anderson <james.r.andreson@utah.edu>
2013-11-06 10:34:04 -0800

    Fixed conflict junk added to file by accident

--- scripts/TEMBuildMosaic.cmd
+++ scripts/TEMBuildMosaic.cmd
@@ -1,6 +1,2 @@
 nornir-build -volume %1 -pipeline TEMPrepare -InputFilter Raw8
-<<<<<<< Updated upstream
 nornir-build -volume %1 -pipeline TEMMosaic -InputFilter Raw8 -AssembleDownsample 8,16,32 -RegistrationDownsample 4 -NoInterlace
-=======
-nornir-build -volume %1 -pipeline TEMMosaic -InputFilter Raw8 -Gamma 1 -AssembleDownsample 8,16,32 -RegistrationDownsample 4 -NoInterlace
->>>>>>> Stashed changes
