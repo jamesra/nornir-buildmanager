26e3663c9833ddd39a3cd8c8b20f5795a062fced
James Anderson <james.r.andreson@utah.edu>
2013-11-21 13:26:31 -0800

    version bump for imageregistration package

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     install_requires = ["nornir_pools>=1.0.2",
                         "nornir_shared>=1.0.1",
-                        "nornir_imageregistration>=1.0.1",
+                        "nornir_imageregistration>=1.0.2",
                         "numpy>=1.7.1",
                         "scipy>=0.12",
                         "matplotlib"]
@@ -30,7 +30,7 @@ if __name__ == '__main__':
 
     dependency_links = ["git+http://github.com/jamesra/nornir-pools#egg=nornir_pools-1.0.2",
                         "git+http://github.com/jamesra/nornir-shared#egg=nornir_shared-1.0.1",
-                        "git+http://github.com/jamesra/nornir-imageregistration#egg=nornir_imageregistration-1.0.1"]
+                        "git+http://github.com/jamesra/nornir-imageregistration#egg=nornir_imageregistration-1.0.2"]
 
     package_dir = {'nornir_buildmanager' : 'nornir_buildmanager'}
     data_files = {'nornir_buildmanager' : ['config/*.xml']}
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     entry_points = {'console_scripts': ['nornir-build = nornir_buildmanager.build:Main']}
 
     setup(name='nornir_buildmanager',
-          version='1.0',
+          version='1.0.1',
           scripts=scripts,
           description="Scripts for the construction of 3D volumes from 2D image sets.",
           author="James Anderson",
