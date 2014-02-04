bb12486208f62393b76bf571a1993c8a7ab2c8e6
James Anderson <james.r.andreson@utah.edu>
2014-02-03 17:03:41 -0800

    Updates to CMP batch files

--- scripts/CMPAlign.cmd
+++ scripts/CMPAlign.cmd
@@ -1,10 +1,9 @@
-nornir-build -volume %1 -pipeline Assemble -Filter LeveledShadingCorrected -AssembleDownsample 1 -NoInterlace -Transform Grid
-nornir-build -volume %1 -pipeline CreateBlobFilter -InputFilter LeveledShadingCorrected -OutputFilter Blob -Levels 1,2,4,8
+nornir-build -volume %1 -pipeline Assemble -Filter LeveledShadingCorrected -AssembleDownsample 2 -NoInterlace -Transform Grid 
+nornir-build -volume %1 -pipeline CreateBlobFilter -InputFilter LeveledShadingCorrected -OutputFilter Blob -Levels 2,4,8
 nornir-build -volume %1 -pipeline AlignSections -Downsample 8 -Filters Blob
 nornir-build -volume %1 -pipeline RefineSectionAlignment -Filters LeveledShadingCorrected -InputGroup StosBrute -InputDownsample 8 -OutputGroup Grid -OutputDownsample 8
 nornir-build -volume %1 -pipeline RefineSectionAlignment -Filters LeveledShadingCorrected -InputGroup Grid -AlignFilters ShadingCorrected -InputDownsample 8 -OutputGroup Grid -OutputDownsample 2
-nornir-build -volume %1 -pipeline ScaleVolumeTransforms -ScaleGroupName Grid -ScaleInputDownsample 4 -ScaleOutputDownsample 1
+nornir-build -volume %1 -pipeline ScaleVolumeTransforms -InputGroup Grid -InputDownsample 2 -OutputDownsample 1
 nornir-build -volume %1 -pipeline SliceToVolume -InputDownsample 1 -InputGroup Grid -OutputGroup SliceToVolume
-#nornir-build -volume %1 -pipeline VolumeImage -VolumeImageGroupName SliceToVolume -VolumeImageDownsample 32
-nornir-build -volume %1 -pipeline MosaicToVolume -InputTransform Grid -OutputTransform ChannelToVolume -Channels TEM
+nornir-build -volume %1 -pipeline MosaicToVolume -InputTransform Grid -OutputTransform ChannelToVolume
 nornir-build -volume %1 -pipeline Assemble -Filter ShadingCorrected -AssembleDownsample 1 -Output %1_Registered -NoInterlace -Transform ChannelToVolume
--- scripts/CMPBuild.cmd
+++ scripts/CMPBuild.cmd
@@ -1,9 +1,9 @@
 nornir-build -volume %1 -input %2 
 nornir-build -volume %1 -pipeline ShadeCorrect -Channels "(?![D|d]api)" -Filters Raw8 -OutputFilter ShadingCorrected -Correction brightfield
-nornir-build -volume %1 -pipeline ShadeCorrect -Channels "(?[D|d]api)" -Filters Raw8 -OutputFilter ShadingCorrected -Correction darkfield
+nornir-build -volume %1 -pipeline ShadeCorrect -Channels "([D|d]api)" -Filters Raw8 -OutputFilter ShadingCorrected -Correction darkfield
 nornir-build -volume %1 -pipeline Prune -InputFilter ShadingCorrected -Downsample 2 -Threshold 1.0
 nornir-build -volume %1 -pipeline Histogram -InputFilter ShadingCorrected -InputTransform Prune -Downsample 2 
 nornir-build -volume %1 -pipeline AdjustContrast -InputFilter ShadingCorrected -OutputFilter LeveledShadingCorrected -InputTransform Prune Leveled -Gamma 1
 nornir-build -volume %1 -pipeline Mosaic -InputFilter LeveledShadingCorrected -RegistrationDownsample 2 -InputTransform Prune -OutputTransform Grid
-nornir-build -volume %1 -pipeline Assemble -Filter ShadingCorrected -AssembleDownsample 1 -Output %1_Output -NoInterlace -Transform Grid
+nornir-build -volume %1 -pipeline Assemble -Filter ShadingCorrected -AssembleDownsample 1 -Output %1_Mosaic -NoInterlace -Transform Grid
 nornir-build -volume %1 -pipeline MosaicReport -PruneFilter Raw8 -ContrastFilter LeveledShadingCorrected -AssembleFilter ShadingCorrected -AssembleDownsample 1
