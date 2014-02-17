b6ad1b302b859aca2d79b8f50fb4e6b112ec841c
James Anderson <james.r.andreson@utah.edu>
2014-02-17 13:59:16 -0800

    Fixed sphinx section header marker line not being as long as the section title text

--- nornir_buildmanager/config/sphinxdocs.py
+++ nornir_buildmanager/config/sphinxdocs.py
@@ -43,7 +43,7 @@ Assemble
        :prog: nornir_build -pipeline Assemble
        
 ExportImages
-----------
+------------
 
     .. argparse:: 
        :module: nornir_buildmanager.config.sphinxdocs
