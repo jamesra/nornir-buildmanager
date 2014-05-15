76c5b76cb3f4533c517845799f32713e4bd588b5
James Anderson <james.r.andreson@utah.edu>
2014-05-15 14:42:07 -0700

    Removed unused script argument

--- scripts/CMPAlign.cmd
+++ scripts/CMPAlign.cmd
@@ -2,7 +2,7 @@ nornir-build Assemble -volume %1 -Filters LeveledShadingCorrected -Downsample 2 
 nornir-build CreateBlobFilter -volume %1  -InputFilter LeveledShadingCorrected -OutputFilter Blob -Levels 2,4,8 -Radius 7 -Median 5 -Max 3
 nornir-build AlignSections -volume %1  -Downsample 8 -Filters Blob
 nornir-build RefineSectionAlignment -volume %1  -Filters LeveledShadingCorrected -InputGroup StosBrute -InputDownsample 8 -OutputGroup Grid -OutputDownsample 8
-nornir-build RefineSectionAlignment -volume %1  -Filters LeveledShadingCorrected -InputGroup Grid -AlignFilters ShadingCorrected -InputDownsample 8 -OutputGroup Grid -OutputDownsample 2
+nornir-build RefineSectionAlignment -volume %1  -Filters LeveledShadingCorrected -InputGroup Grid -InputDownsample 8 -OutputGroup Grid -OutputDownsample 2
 nornir-build ScaleVolumeTransforms -volume %1  -InputGroup Grid -InputDownsample 2 -OutputDownsample 1
 nornir-build SliceToVolume -volume %1  -InputDownsample 1 -InputGroup Grid -OutputGroup SliceToVolume
 nornir-build MosaicToVolume -volume %1  -InputTransform Grid -OutputTransform ChannelToVolume
