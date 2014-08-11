04f76e65c55c60c82a4205b109631b60a4a2955e
jamesra <james.r.anderson@utah.edu>
2014-08-11 13:04:24 -0700

    Allow output name for reports to be specified

--- scripts/TEMBuild.cmd
+++ scripts/TEMBuild.cmd
@@ -3,5 +3,5 @@ nornir-build Histogram -volume %1 -Filters Raw8 -InputTransform Prune -Downsampl
 nornir-build AdjustContrast -volume %1 -InputFilter Raw8 -OutputFilter Leveled -InputTransform Prune -Channels TEM
 nornir-build Mosaic -volume %1 -InputFilter Leveled -RegistrationDownsample 4 -InputTransform Prune -OutputTransform Grid -Channels TEM 
 nornir-build Assemble -volume %1 -Channels TEM -Filters Leveled -Downsample 8,16,32 -NoInterlace -Transform Grid
-nornir-build MosaicReport -volume %1 -PruneFilter Raw8 -ContrastFilter Raw8 -AssembleFilter Leveled -AssembleDownsample 16
+nornir-build MosaicReport -volume %1 -PruneFilter Raw8 -ContrastFilter Raw8 -AssembleFilter Leveled -AssembleDownsample 16 -Output MosaicReport
 nornir-build CreateVikingXML -volume %1 -OutputFile Mosaic
