33016decbf197bbfba1e82ca2c9e4a5efac0e6ac
James Anderson <james.r.andreson@utah.edu>
2014-09-10 14:38:45 -0700

    Update console title when running the included idoc build scripts

--- scripts/TEMBuild.cmd
+++ scripts/TEMBuild.cmd
@@ -1,7 +1,14 @@
+title Prune
 nornir-build Prune -volume %1 -InputFilter Raw8 -Downsample 4 -Channels TEM -Threshold 10.0
-nornir-build Histogram -volume %1 -Filters Raw8 -InputTransform Prune -Downsample 4 -Channels TEM 
+title Histogram
+nornir-build Histogram -volume %1 -Filters Raw8 -InputTransform Prune -Downsample 4 -Channels TEM
+title AdjustContrast 
 nornir-build AdjustContrast -volume %1 -InputFilter Raw8 -OutputFilter Leveled -InputTransform Prune -Channels TEM
-nornir-build Mosaic -volume %1 -InputFilter Leveled -RegistrationDownsample 4 -InputTransform Prune -OutputTransform Grid -Channels TEM 
+title Mosaic
+nornir-build Mosaic -volume %1 -InputFilter Leveled -RegistrationDownsample 4 -InputTransform Prune -OutputTransform Grid -Channels TEM
+title Assemble 
 nornir-build Assemble -volume %1 -Channels TEM -Filters Leveled -Downsample 8,16,32 -NoInterlace -Transform Grid
+title MosaicReport
 nornir-build MosaicReport -volume %1 -PruneFilter Raw8 -ContrastFilter Raw8 -AssembleFilter Leveled -AssembleDownsample 16 -Output MosaicReport
+title CreateVikingXML
 nornir-build CreateVikingXML -volume %1 -OutputFile Mosaic
