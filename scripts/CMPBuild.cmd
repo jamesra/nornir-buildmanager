6641a390a9e1f0b63c196784058f54850f8e8019
James Anderson <james.r.andreson@utah.edu>
2014-09-12 15:55:51 -0700

    Add titles to console window indicating current pipeline

--- scripts/CMPBuild.cmd
+++ scripts/CMPBuild.cmd
@@ -1,10 +1,20 @@
-nornir-build import %2 -volume %1 
+title Import
+nornir-build import %2 -volume %1
+title ShadeCorrect 
 nornir-build ShadeCorrect -volume %1 -Channels "(?![D|d]api)" -Filters Raw8 -OutputFilter ShadingCorrected -Correction brightfield
+title ShadeCorrect
 #nornir-build ShadeCorrect -volume %1  -Channels "([D|d]api)" -Filters Raw8 -OutputFilter ShadingCorrected -Correction darkfield
+title Prune
 nornir-build Prune -volume %1   -InputFilter ShadingCorrected -Downsample 2 -Threshold 1.0
-nornir-build Histogram -volume %1  -Filters ShadingCorrected -InputTransform Prune -Downsample 2 
+title Histogram
+nornir-build Histogram -volume %1  -Filters ShadingCorrected -InputTransform Prune -Downsample 2
+title AdjustContrast 
 nornir-build AdjustContrast -volume %1  -InputFilter ShadingCorrected -OutputFilter LeveledShadingCorrected -InputTransform Prune -Gamma 1
+title Mosaic
 nornir-build Mosaic -volume %1  -InputFilter LeveledShadingCorrected -RegistrationDownsample 2 -InputTransform Prune -OutputTransform Grid
+title Assemble
 nornir-build Assemble -volume %1  -Filters ShadingCorrected -Downsample 1 -NoInterlace -Transform Grid
+title ExportImages
 nornir-build ExportImages -volume %1  -Filters ShadingCorrected -Downsample 1 -Output %1_Mosaic
+title MosaicReport
 nornir-build MosaicReport -volume %1  -PruneFilter ShadingCorrected -ContrastFilter Raw8 -AssembleFilter ShadingCorrected -AssembleDownsample 1 -Output ImageReport
--- scripts/TEMPrepare.cmd
+++ scripts/TEMPrepare.cmd
@@ -1,3 +1,6 @@
-nornir-build import %2 -volume %1  
+title Import %1
+nornir-build import %2 -volume %1
+title Prune
 nornir-build Prune -volume %1 -InputFilter Raw8 -Downsample 4 -Channels TEM -Threshold 10.0
+title Histogram
 nornir-build Histogram -volume %1 -Filters Raw8 -InputTransform Prune -Downsample 4 -Channels TEM 
