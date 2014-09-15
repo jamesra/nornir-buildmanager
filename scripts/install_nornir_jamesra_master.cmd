ae672114150e2269b113fca1a869b3690c4b953b
James Anderson <james.r.andreson@utah.edu>
2014-09-15 13:55:20 -0700

    Add scripts to install/upgrade/uninstall nornir and dev branches

---
+++ scripts/install_nornir_jamesra_master.cmd
@@ -0,0 +1,4 @@
+pip install git+https://github.com/jamesra/nornir-shared.git --upgrade
+pip install git+https://github.com/jamesra/nornir-pools.git --upgrade
+pip install git+https://github.com/jamesra/nornir-imageregistration.git --upgrade
+pip install git+https://github.com/jamesra/nornir-buildmanager.git --upgrade
---
+++ scripts/uninstall_nornir.cmd
@@ -0,0 +1,4 @@
+pip uninstall nornir-buildmanager -y
+pip uninstall nornir-imageregistration -y
+pip uninstall nornir-pools -y
+pip uninstall nornir-shared -y
