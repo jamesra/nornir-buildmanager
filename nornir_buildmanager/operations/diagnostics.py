487bc6bebaa0f373c4ff1d9141ea095007db9fde
James Anderson <james.r.anderson@utah.edu>
2019-02-13 14:31:57 -0800

    Added pipeline to dump tilesets. Tweaked number of relax iterations parameter

--- nornir_buildmanager/operations/diagnostics.py
+++ nornir_buildmanager/operations/diagnostics.py
@@ -11,6 +11,11 @@ import nornir_shared.files
 
 num_contrast_pad_chars = 64
 
+def Print(val, **kwargs):
+    '''Allows the Pipelines.xml to print an arbitrary string to the console'''
+    print(val)
+    return None
+
 def PrintContrastValuesHeader(**kwargs):
     global num_contrast_pad_chars
     print("Path%sMin    \tMax    \tGamma" % (' ' * num_contrast_pad_chars))
@@ -39,6 +44,38 @@ def PrintContrastValues(node, **kwargs):
     
     print("%s\t%s\t%s\t%s" % (pathstr, minStr, maxStr, gammaStr))
     return None
+
+def PrintIfMissingTileset(filter_node, **kwargs):
+    '''Print a comma delimited set of attributes from the node in order.  Indent the output according to tab_indent_count'''
+    
+    if filter_node.HasTileset():
+        return
+
+    section_node = filter_node.GetParent("Section")        
+    channel_node = filter_node.GetParent("Channel")
+            
+    print("%s\t%s\t%s\t%s" % (section_node.Number, channel_node.Name, filter_node.Name))
+
+def PrintAttributes(node, attribs=None, tab_indent_count=None, **kwargs):
+    '''Print a comma delimited set of attributes from the node in order.  Indent the output according to tab_indent_count'''
+    
+    attrib_list = attribs.split(',')
+    
+    if tab_indent_count is None:
+        tab_indent_count = 0
+        
+    tabs = '\t' * tab_indent_count
+    
+    output_str = tabs
+    
+    for attrib in attrib_list:
+        if attrib in node:
+            val = getattr(node, attrib)
+            output_str.append("\t{0}".format(str(val)))
+        else:
+            output_str.append("\t\t".format(str(val))) 
+        
+    print(output_str)
 
 def GetNewestTile(level_node):
     files = sorted([os.path.join(level_node.FullPath, x) for x in os.listdir(level_node.FullPath)], key=os.path.getctime, reverse=True)
