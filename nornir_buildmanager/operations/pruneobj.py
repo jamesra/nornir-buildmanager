b9bbbaf863c76af022287954ab559c12c21958bf
jameskuch <james.kuchenbecker@gmail.com>
2019-06-20 18:15:47 -0700

    Remove prune.mosaics with the wrong type or threshold value

--- nornir_buildmanager/operations/pruneobj.py
+++ nornir_buildmanager/operations/pruneobj.py
@@ -100,7 +100,7 @@ class PruneObj:
         # Check if there is an existing prune map, and if it exists if it is out of date
         PruneNodeParent = PruneNode.Parent
         
-        transforms.RemoveWhere(TransformParent, 'Transform[@Name="' + OutputTransformName + '"]', lambda t: t.Threshold != Threshold)
+        transforms.RemoveWhere(TransformParent, 'Transform[@Name="' + OutputTransformName + '"]', lambda t: (t.Threshold != Threshold) or (t.Type != MangledName))
         
         '''TODO: Add function to remove duplicate Prune Transforms with different thresholds'''
 
