29dd33b554705e327092f2b02a40fa05bc9868b9
James Anderson <james.r.anderson@utah.edu>
2022-10-26 14:20:57 -0700

    Fixed not being able to import a path with an .idoc in it.  Importing paths with child folders with .idocs was working.

--- nornir_buildmanager/importers/idoc.py
+++ nornir_buildmanager/importers/idoc.py
@@ -59,7 +59,7 @@ import nornir_buildmanager.importers.serialem_utils as serialem_utils
 from nornir_buildmanager.importers.serialemlog import SerialEMLog
 
 
-def find_section_candidates(ImportPath: str, DesiredSectionList: list[int] | None ) -> dict[int, list[shared.FilenameMetadata]]:
+def find_section_candidates(ImportPath: str, DesiredSectionList: list[int] | None) -> dict[int, list[shared.FilenameMetadata]]:
     """
     This fetches the directories that could contain sections so that when we recurse through the section folders we can
     yield the sections we find right away instead of waiting for all sections to be found first
@@ -71,13 +71,21 @@ def find_section_candidates(ImportPath: str, DesiredSectionList: list[int] | Non
 
     found_sections = {}  # type : dict[int, list[shared.FilenameMetadata]]
 
+    # Checking for .idocs here handles the case of directly importing the section directory instead of the parent directory
+    try:
+        root_meta_data = shared.GetSectionInfo(ImportPath)
+        found_sections[root_meta_data.number] = [root_meta_data]
+    except nornir_buildmanager.NornirUserException:
+        #This means we are going to check child directory names only and not this folder for idocs
+        root_meta_data = None
+
     with os.scandir(ImportPath) as scanner:
         for entry in scanner:
-            if entry.is_dir() is False:
+            if not entry.is_dir():
                 continue
 
             try:
-                meta_data = shared.GetSectionInfo(entry.name)
+                meta_data = shared.GetSectionInfo(os.path.join(ImportPath, entry.name))
             except nornir_buildmanager.NornirUserException:
                 prettyoutput.LogErr(f"Could not parse required metadata from {entry.name}")
                 continue
@@ -94,34 +102,33 @@ def find_section_candidates(ImportPath: str, DesiredSectionList: list[int] | Non
                     found_sections[meta_data.number].append(meta_data)
                 else:
                     found_sections[meta_data.number] = [meta_data]
-                    
+
     #Sort the lists by the version number
     for key, candidate_data_list in found_sections.items():
         found_sections[key] = sorted(candidate_data_list, key=lambda entry: entry.version, reverse=True)
 
     return found_sections
 
-def find_section_directory_idocs(ImportPath: str, section_meta_data: shared.FilenameMetadata, matching_pattern: re.Pattern | frozenset[str]) -> tuple[shared.FilenameMetadata, list[str]]:
+def find_section_directory_idocs(section_meta_data: shared.FilenameMetadata, matching_pattern: re.Pattern | frozenset[str]) -> tuple[shared.FilenameMetadata, list[str]]:
     """
     :param ImportPath:
     :param section_meta_data:
     :param matching_pattern:
     :return: yields all idoc files for a given section directory
     """
-    #matching_pattern = nornir_shared.files.ensure_regex_or_set(extension)
-    section_path = os.path.join(ImportPath, section_meta_data.fullpath)
+    #matching_pattern = nornir_shared.files.ensure_regex_or_set(extension) 
     matched_file_list = []
-    with os.scandir(section_path) as path_scanner:
+    with os.scandir(section_meta_data.fullpath) as path_scanner:
         for entry in path_scanner:
             if entry.is_file() is False:
                 continue
 
             if nornir_shared.files.check_if_file_matches(entry.name, matching_pattern):
-                matched_file_list.append(os.path.join(section_path, entry.name))
+                matched_file_list.append(os.path.join(section_meta_data.fullpath, entry.name))
 
     return section_meta_data, matched_file_list
 
-def section_directory_idocs_generator(ImportPath: str, match_names: list[shared.FilenameMetadata], matching_pattern: re.Pattern | frozenset[str]) -> Generator[tuple[shared.FilenameMetadata, list[str]]]:
+def section_directory_idocs_generator(match_names: list[shared.FilenameMetadata], matching_pattern: re.Pattern | frozenset[str]) -> Generator[tuple[shared.FilenameMetadata, list[str]]]:
     """
     :param ImportPath:
     :param match_names:
@@ -130,7 +137,7 @@ def section_directory_idocs_generator(ImportPath: str, match_names: list[shared.
     """
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        yield from executor.map(lambda section_meta_data: find_section_directory_idocs(ImportPath, section_meta_data, matching_pattern), match_names)
+        yield from executor.map(lambda section_meta_data: find_section_directory_idocs(section_meta_data, matching_pattern), match_names)
 
 
 def find_sections(ImportPath: str, extension: str, section_candidates: dict[int, list[shared.FilenameMetadata]]) -> dict[
@@ -144,7 +151,7 @@ def find_sections(ImportPath: str, extension: str, section_candidates: dict[int,
 
     best_section_results = {} # type: dict[int, tuple[shared.FilenameMetadata, list[str]]]
 
-    for section_meta_data, idocFileList in section_directory_idocs_generator(ImportPath, match_names, matching_pattern):
+    for section_meta_data, idocFileList in section_directory_idocs_generator(match_names, matching_pattern):
         # The problem with using the directory name is that there could be more than one
         # .idoc file in a directory if a two-part capture of a section is done.
         # However, the extensive use of 1.idoc naming
